f0b74fdf4db3f10fc483697c4086b728d6a27d01
Aman Hingu <amanhingu@Amans-MacBook-Pro-2.local>
2022-04-13 10:09:47 -0400

    Finished code for presentation

--- Data Exploration/Practice:Planning.R
+++ Data Exploration/Practice:Planning.R
@@ -30,12 +30,80 @@ summary(chi_AIC)
 step(chi_AIC)
 
 library(corrplot)
-chi_select <-chi_na.omit %>%
+chi_select_1 <-chi_na.omit %>%
+  select(LowBi_ight, Unemp_ment, Below_evel, NoHig_loma, comm_area)
+
+chi_select_2 <-chi_na.omit %>%
   select(LowBi_ight, Unemp_ment, Below_evel, NoHig_loma)
 
-chi_cor <-cor(chi_select)
+
+chi_cor <-cor(chi_select_2)
 corrplot(chi_cor)
+c
 
 
 chi_plot <-ggplot(chi_na.omit, aes(x=Below_evel, y=LowBi_ight, color = comm_area))+geom_point()
 print(chi_plot)
+
+
+#make a heat/density map of low birth weight prevalence across chicago community areas 
+
+ggplot()+geom_sf(data=chi_h_ind.sf, aes(fill= LowBi_ight))+ scale_fill_continuous(name="Low Birth Weight Prevalence")+
+  ggtitle("Low Birth Weight Prevalence in Chicago Community Areas", subtitle = "Aman Hingu")
+
+
+
+summary(chi_h_ind.df)
+#no na's in dataset
+
+#make single linear regressions with each of the variables
+
+chi_select_lm1 <-lm(data = chi_select_1, LowBi_ight ~ Unemp_ment)
+chi_select_lm1
+summary(chi_select_lm1)
+
+par(mfrow = c(2,2), mar=c(4,4,4,4))
+plot(chi_select_lm1)
+par(mfrow = c(1,1))
+
+lm1_plot <-ggplot(chi_select_1, aes(x=Unemp_ment, y=LowBi_ight))+geom_point()
+print(lm1_plot)
+
+chi_select_lm2 <-lm(data=chi_select_1, LowBi_ight ~ Below_evel)
+chi_select_lm2
+summary(chi_select_lm2)
+
+par(mfrow = c(2,2), mar=c(4,4,4,4))
+plot(chi_select_lm2)
+par(mfrow = c(1,1))
+
+lm1_plot <-ggplot(chi_select_2, aes(x=Below_evel, y=LowBi_ight))+geom_point()
+print(lm1_plot)
+
+chi_select_lm3 <-lm(data=chi_select_1, LowBi_ight ~ NoHig_loma)
+chi_select_lm3
+summary(chi_select_lm3)
+
+par(mfrow = c(2,2), mar=c(4,4,4,4))
+plot(chi_select_lm3)
+par(mfrow = c(1,1))
+
+lm1_plot <-ggplot(chi_select_3, aes(x=NoHig_loma, y=LowBi_ight))+geom_point()
+print(lm1_plot)
+
+#do a multiple linear regression
+
+chi_select_lm4 <-lm(data=chi_select_1, LowBi_ight ~ Unemp_ment + Below_evel + NoHig_loma)
+summary(chi_select_lm4)
+
+
+par(mfrow = c(2,2), mar=c(4,4,4,4))
+plot(chi_select_lm4)
+par(mfrow = c(1,1))
+
+chi_select_lm5 <-lm(data=chi_select_1, LowBi_ight ~ Unemp_ment + Below_evel)
+summary(chi_select_lm5)
+par(mfrow = c(2,2), mar=c(4,4,4,4))
+plot(chi_select_lm4)
+par(mfrow = c(1,1))
+
