18fdf05e0c948a4018d2a4034ecc9a95d7b494f2
Aman Hingu <amanhingu@Amans-MBP-2.attlocal.net>
2022-04-20 22:02:32 -0400

    Fixed issues with R Markdown tables, plots being cut off, R chunks appearing in the wrong order. Final report, finalized code file, and Latex files are all updated.

--- Finalized Code /Code for Final Report.R
+++ Finalized Code /Code for Final Report.R
@@ -13,41 +13,16 @@ library(mapview)
 sf::sf_use_s2(FALSE)
 
 
-
-
 getwd()
 setwd("/Users/amanhingu/Documents/Hingu_ENV872_EDA_FinalProject")
 
-chi_h_ind.sf <-st_read('./healthIndicators/HealthIndicators.shp')
+chi_h_ind.sf <-st_read('./Raw Data/healthIndicators_Raw/HealthIndicators.shp')
 mapview(chi_h_ind.sf)
 ggplot(data=chi_h_ind.sf)+geom_sf()
 head(chi_h_ind.sf, n=10)
+dim(chi_h_ind.df)
+summary(chi_h_ind.sf)
 chi_h_ind.df <- as.data.frame(chi_h_ind.sf)
-#explore
-
-chi_na.omit <-chi_h_ind.df %>%
-  na.omit()
-chi_AIC <-lm(data=chi_na.omit, LowBi_ight ~ Unemp_ment+Below_evel+NoHig_loma)
-summary(chi_AIC)
-step(chi_AIC)
-
-library(corrplot)
-chi_select_1 <-chi_na.omit %>%
-  select(LowBi_ight, Unemp_ment, Below_evel, NoHig_loma, comm_area)
-
-chi_select_2 <-chi_na.omit %>%
-  select(LowBi_ight, Unemp_ment, Below_evel, NoHig_loma)
-
-write.csv(chi_na.omit, file = "./Processed Data Folder/chi_na.omit.csv", row.names=FALSE)
-write.csv(chi_select_2, file = "./Processed Data Folder/chi_select_2.csv", row.names=FALSE)
-
-chi_cor <-cor(chi_select_2)
-corrplot(chi_cor)
-c
-
-
-chi_plot <-ggplot(chi_na.omit, aes(x=Below_evel, y=LowBi_ight, color = comm_area))+geom_point()
-print(chi_plot)
 
 
 #make a heat/density map of low birth weight prevalence across chicago community areas
@@ -66,53 +41,47 @@ ggplot()+geom_sf(data=chi_h_ind.sf, aes(fill= Below_evel))+ scale_fill_continuou
 ggplot()+geom_sf(data=chi_h_ind.sf, aes(fill= NoHig_loma))+ scale_fill_continuous(name="Educational Attainment")+
   ggtitle("Educational Attainment in Chicago Community Areas", subtitle = "Aman Hingu")
 
-summary(chi_h_ind.df)
-#no na's in dataset
 
-#make single linear regressions with each of the variables
+#use corrplot and AIC to further justify running LM's
 
-chi_select_lm1 <-lm(data = chi_select_1, LowBi_ight ~ Unemp_ment)
-chi_select_lm1
-summary(chi_select_lm1)
+library(corrplot)
+chi_select_1 <-chi_na.omit %>%
+  select(LowBi_ight, Unemp_ment, Below_evel, NoHig_loma, comm_area)
 
-par(mfrow = c(2,2), mar=c(4,4,4,4))
-plot(chi_select_lm1)
-par(mfrow = c(1,1))
+chi_select_2 <-chi_na.omit %>%
+  select(LowBi_ight, Unemp_ment, Below_evel, NoHig_loma)
+
+write.csv(chi_na.omit, file = "./Processed Data Folder/chi_na.omit.csv", row.names=FALSE)
+write.csv(chi_select_2, file = "./Processed Data Folder/chi_select_2.csv", row.names=FALSE)
+
+chi_cor <-cor(chi_select_2)
+corrplot(chi_cor)
+
+chi_AIC <-lm(data=chi_select_2, LowBi_ight ~ Unemp_ment+Below_evel+NoHig_loma)
+summary(chi_AIC)
+step(chi_AIC)
+
+
+#scatter plots of each independent variable
 
 lm1_plot <-ggplot(chi_select_1, aes(x=Unemp_ment, y=LowBi_ight))+geom_point()+
   labs(title = "Low Birth Weight Prevalence vs. Unemployment", x ='Unemployment', 
-y ='Low Birth Weight Prevalence') +geom_smooth(method = "lm", color ="blue")+ah__theme
+       y ='Low Birth Weight Prevalence') +geom_smooth(method = "lm", color ="blue")+ah__theme
 print(lm1_plot)
 
-chi_select_lm2 <-lm(data=chi_select_1, LowBi_ight ~ Below_evel)
-chi_select_lm2
-summary(chi_select_lm2)
-
-par(mfrow = c(2,2), mar=c(4,4,4,4))
-plot(chi_select_lm2)
-par(mfrow = c(1,1))
-
-lm1_plot <-ggplot(chi_select_2, aes(x=Below_evel, y=LowBi_ight))+geom_point()
+lm2_plot <-ggplot(chi_select_1, aes(x=Unemp_ment, y=Below_evel))+geom_point()+
+  labs(title = "Low Birth Weight Prevalence vs. Portion under Poverty Level", x ='Portion under Poverty Level', 
+       y ='Low Birth Weight Prevalence') +geom_smooth(method = "lm", color ="blue")+ah__theme
 print(lm1_plot)
 
-chi_select_lm3 <-lm(data=chi_select_1, LowBi_ight ~ NoHig_loma)
-chi_select_lm3
-summary(chi_select_lm3)
-
-par(mfrow = c(2,2), mar=c(4,4,4,4))
-plot(chi_select_lm3)
-par(mfrow = c(1,1))
-
-lm1_plot <-ggplot(chi_select_3, aes(x=NoHig_loma, y=LowBi_ight))+geom_point()
+lm3_plot <-ggplot(chi_select_1, aes(x=Unemp_ment, y=NoHig_loma))+geom_point()+
+  labs(title = "Low Birth Weight Prevalence vs. Portion under Poverty Level", x ='Educational Attainment', 
+       y ='Low Birth Weight Prevalence') +geom_smooth(method = "lm", color ="blue")+ah__theme
 print(lm1_plot)
 
-#do a multiple linear regression
 
-chi_select_lm4 <-lm(data=chi_select_1, LowBi_ight ~ Unemp_ment + Below_evel + NoHig_loma)
-summary(chi_select_lm4)
-par(mfrow = c(2,2), mar=c(4,4,4,4))
-plot(chi_select_lm4)
-par(mfrow = c(1,1))
+#run multivariable linear regression models
+ #first model includes unemployment and portion under poverty level based off recommendations from the corrplot
 
 chi_select_lm5 <-lm(data=chi_select_1, LowBi_ight ~ Unemp_ment + Below_evel)
 summary(chi_select_lm5)
@@ -121,9 +90,16 @@ plot(chi_select_lm4)
 par(mfrow = c(1,1))
 
 
+ #second model includes all three variables in the interest of increasing model fit while maintaining significance
+
+chi_select_lm4 <-lm(data=chi_select_1, LowBi_ight ~ Unemp_ment + Below_evel + NoHig_loma)
+summary(chi_select_lm4)
+par(mfrow = c(2,2), mar=c(4,4,4,4))
+plot(chi_select_lm4)
+par(mfrow = c(1,1))
 
 
 
 
 
-           
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Hingu_ENV872_EDA_FinalProject
 Repository for Aman Hingu's Final Project for ENV872
+
+The title of this project is "Low Birth Weight Prevalence and Socioeconomic Factors in Chicago", by Aman Hingu. For this project, the dataset used was the Chicago Health Indicators dataset which has public health and socioeconomic indicator data for 77 community areas of the greater Chicago Metropolitan Area. The link to this dataset can be found below:
+https://geodacenter.github.io/data-and-lab//healthindicators-variables/
+
+There is a strong amount of previous work done discussing the effects of maternal nutrition, birth order, and substance use on the likelihood of low birth weight. Outside the context of looking at mothers, it has been observed that factors such as one’s geographic location within an urban city can affect their health as it can effect their access to nutrient dense food vendors, and proximity to high quality healthcare. Where someone lives alone can sometimes be an indicator of their socioeconomic status. 
+
+The research question of this project is exploring whether socioeconomic factors such as unemployment, educational attainment, and portion under poverty level can explain the variability in low live birth weight prevalence across the Chicago community areas.
